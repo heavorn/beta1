0bbfc557826739bb08a8bda0a6843d233516cdc5
Joseph <cheasovorn4@gmail.com>
2023-09-18 11:13:24 +0800

    Intergrate YOLO-MS

--- nn/modules/__init__.py
+++ nn/modules/__init__.py
@@ -13,11 +13,11 @@ torch.onnx.export(m, x, f)
 os.system(f'onnxsim {f} {f} && open {f}')
 """
 
-from .block import (DFL, SPPF, Bottleneck, C2f)
+from .block import (DFL, SPPF, Bottleneck, C2f, MSBlockLayer, MSBlock, MSBlock_D)
 from .conv import (CBAM, ChannelAttention, Concat, Conv, Conv2, ConvTranspose, DWConv, DWConvTranspose2d, Focus,
                    GhostConv, LightConv, RepConv, SpatialAttention)
 from .head import Detect
 
 __all__ = ('Conv', 'Conv2', 'LightConv', 'RepConv', 'DWConv', 'DWConvTranspose2d', 'ConvTranspose', 'Focus',
            'GhostConv', 'ChannelAttention', 'SpatialAttention', 'CBAM', 'Concat', 'DFL', 'SPPF',
-           'C2f', 'Bottleneck', 'Detect', 'Segment', 'Pose', 'Classify')
+           'C2f', 'Bottleneck', 'Detect', 'Segment', 'Pose', 'Classify', 'MSBlockLayer', 'MSBlock', 'MSBlock_D')
